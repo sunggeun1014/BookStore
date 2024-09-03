8500d03125cf0113debbf424815b9eafb40c2089
dohyeon <dojin8351@gmail.com>
2024-09-03 18:52:55 +0900

    Feat : 로그인시 오류 메시지 출력, 매니저 등록 비밀번호,아이디 양식 추가

---
+++ src/main/java/com/ezen/bookstore/admin/security/service/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,39 @@
+package com.ezen.bookstore.admin.security.service;
+
+
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+
+        String errorMessage = "아이디 또는 비밀번호가 일치하지 않습니다.";
+        String redirectUrl = "/admin/login?error=true";
+
+        if (exception instanceof UsernameNotFoundException) {
+            if ("접근 권한이 없습니다.".equals(exception.getMessage())) {
+                redirectUrl += "&accessError=" + URLEncoder.encode(exception.getMessage(), "UTF-8");
+            } else {
+                redirectUrl += "&usernameError=" + URLEncoder.encode(exception.getMessage(), "UTF-8");
+            }
+        } else if (exception instanceof BadCredentialsException) {
+            redirectUrl += "&passwordError=" + URLEncoder.encode(errorMessage, "UTF-8");
+        }
+
+        response.sendRedirect(redirectUrl);
+    }
+}
+
+
