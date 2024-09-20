6bcfc4760f85b752e582a6b5dab07a99a01e11d5
kimssi9567 <kimssi9567@gmail.com>
2024-09-20 11:30:36 +0900

    Fix: 어드민 주문관리 - 배송지정보, 회수지 정보 이름/전화번호 추가

--- src/main/java/com/ezen/bookstore/admin/customerorders/dto/CustomerOrdersDTO.java
+++ src/main/java/com/ezen/bookstore/admin/customerorders/dto/CustomerOrdersDTO.java
@@ -7,16 +7,20 @@ import lombok.Data;
 @Data
 public class CustomerOrdersDTO {
 	private Integer order_num;        
-	private String order_addr ;
-	private String order_addr_detail; 
+	private String order_addr;
+	private String order_addr_detail;
+	private String recipient_name;
+	private String recipient_phoneNo;
 	private String common_ent_pw;
 	private String order_delivery_status;       
 	private String order_payment_status;     
 	private Timestamp order_purchase_date;      
-	private Timestamp order_modify_date;          
+	private Timestamp order_modify_date;
 	private String member_id;
 	private String retrieve_addr;
 	private String retrieve_addr_detail;
+	private String retrieve_name;
+	private String retrieve_phoneNo;
 	
 	private Integer order_detail_num;       
 	private Integer order_detail_qty;       
