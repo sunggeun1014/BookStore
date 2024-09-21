22ee8db77a46798cae5376fe41e547dd9d8e7f86
dohyeon <dojin8351@gmail.com>
2024-09-21 18:55:43 +0900

    Feat : 공지사항 상세 페이지 완료.

--- src/main/java/com/ezen/bookstore/user/mypage/notice/mapper/UserNoticesMapper.java
+++ src/main/java/com/ezen/bookstore/user/mypage/notice/mapper/UserNoticesMapper.java
@@ -3,10 +3,15 @@ package com.ezen.bookstore.user.mypage.notice.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.ezen.bookstore.user.mypage.notice.dto.UserNoticesDTO;
 
 @Mapper
 public interface UserNoticesMapper {
-	List<UserNoticesDTO> getNoticesList(UserNoticesDTO usernoticesDTO);	
+	List<UserNoticesDTO> getNoticesList(UserNoticesDTO usernoticesDTO);
+	UserNoticesDTO getNoticesDetail(Integer noticeNum);
+    UserNoticesDTO getPreviousNotice(@Param("noticeNum") Integer noticeNum);
+    UserNoticesDTO getNextNotice(@Param("noticeNum") Integer noticeNum);
+
 }
